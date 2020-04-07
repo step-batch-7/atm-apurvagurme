1bcb5997aa2f850ac3e1e51c79a182220971f0f1
Apurva <58025903+apurvagurme@users.noreply.github.com>
2020-04-07 20:20:11 +0530

    Added more amounts in array

--- main.c
+++ main.c
@@ -3,18 +3,6 @@
 
 void display_notes(unsigned int);
 
-int main(void)
-{
-  unsigned short int amounts[] = {31999};
-  for (int i = 0; i < 1; i++)
-  {
-    unsigned int notes = get_money(amounts[i]);
-    display_notes(notes);
-    printf("%5u %08x\n", amounts[i], notes);
-  }
-  return 0;
-}
-
 void display_notes(unsigned int denominations)
 {
   int notes[8] = {2000, 500, 100, 50, 20, 10, 5, 1};
@@ -24,4 +12,16 @@ void display_notes(unsigned int denominations)
     denominations = denominations << 4;
     printf("%d notes of %d\n", count, notes[index]);
   }
+}
+
+int main(void)
+{
+  unsigned short int amounts[] = {1, 10, 39, 589, 2590, 31999};
+  for (int i = 0; i < 6; i++)
+  {
+    unsigned int notes = get_money(amounts[i]);
+    display_notes(notes);
+    printf("%5u %08x\n", amounts[i], notes);
+  }
+  return 0;
 }
